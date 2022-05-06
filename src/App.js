e06ab0ac3a640845b384ac2e3c127fcd94cf2ad0
Zunaid-jony <jony35666666@gmail.com>
2022-05-07 00:04:44 +0600

    first commit update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componets/Header/Header';
+import Benar from './componets/Header/benar/Benar';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Benar></Benar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <nav class="navbar-brand">Navbar</nav>
+                
+                <nav class="navbar-brand">Sing up</nav>
+                
+                
+            </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
